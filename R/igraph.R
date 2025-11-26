888146abd31aad44f7950e484bfac48c5a9f8b6f
Mitchell O'Hara-Wild <mail@mitchelloharawild.com>
2025-11-26 19:53:11 +1100

    Add as.igraph methods

--- R/igraph.R
+++ R/igraph.R
@@ -1,13 +1,28 @@
-graphvec_to_igraph <- function(g) {
+#' @method as.igraph node_vec
+#' @export
+#' @importFrom igraph as.igraph graph_from_edgelist
+as.igraph.node_vec <- function(x) {
   # Temporary design hack to allow for direct passing of combined graph
-  if(!all(c("from", "to") %in% names(g))) {
-    if(inherits(g, c("agg_vec", "node_vec"))) {
-      g <- list(g)
-    }
-    g <- combine_graph(g)
-  }
+  # if(!all(c("from", "to") %in% names(g))) {
+  #   if(inherits(g, c("agg_vec", "node_vec"))) {
+  #     g <- list(g)
+  #   }
+  #   g <- combine_graph(g)
+  # }
+
+  e <- attr(x, "edges")
 
   graph_from_edgelist(
-    cbind(from = unlist(g[["from"]]), to = rep(g[["to"]], lengths(g[["from"]])))
+    cbind(from = unlist(e[["from"]]), to = rep(e[["to"]], lengths(e[["from"]])))
+  )
+}
+
+#' @method as.igraph edge_vec
+#' @export
+#' @importFrom igraph as.igraph graph_from_edgelist
+as.igraph.edge_vec <- function(x) {
+  e <- vec_data(x)
+  graph_from_edgelist(
+    cbind(from = e[["from"]], to = e[["to"]])
   )
 }
