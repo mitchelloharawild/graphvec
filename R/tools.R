6a3c567b49aedfbdc96cfaf5a8c0f7bcd5ac34c4
Mitchell O'Hara-Wild <mail@mitchelloharawild.com>
2025-11-26 19:53:27 +1100

    rm node_* functions (defer to as.igraph methods)

--- R/tools.R
+++ R/tools.R
@@ -4,25 +4,23 @@
 #' @param node The root node from which children are identified
 #'
 #' @importFrom igraph graph_from_edgelist subcomponent
-#' @export
 node_is_child <- function(x, node, ...){
   stopifnot(inherits(x, "node_vec"))
-  edges <- attr(x, "edges")
-  edges <- graph_from_edgelist(
+  g <- attr(x, "edges")
+  g <- graph_from_edgelist(
     cbind(from = unlist(g[["from"]]), to = rep(g[["to"]], lengths(g[["from"]])))
   )
+
   unclass(x) %in% subcomponent(g, match(node, levels(x)), mode = "in")
 }
 
 #' Identify the degree of a node in a graph
 #'
 #' @param x A graph vector
-#'
-#' @export
 node_degree <- function(x, ...){
   stopifnot(inherits(x, "node_vec"))
-  edges <- attr(x, "edges")
-  edges <- graph_from_edgelist(
+  g <- attr(x, "edges")
+  g <- graph_from_edgelist(
     cbind(from = unlist(g[["from"]]), to = rep(g[["to"]], lengths(g[["from"]])))
   )
   igraph::degree(g)[vec_data(x)]
@@ -32,13 +30,11 @@ node_degree <- function(x, ...){
 #' Identify the distance from a node in a graph
 #'
 #' @param x A graph vector
-#'
-#' @export
 node_distance <- function(x, from, ...){
   stopifnot(inherits(x, "node_vec"))
   from <- match(from, levels(x))
-  edges <- attr(x, "edges")
-  edges <- graph_from_edgelist(
+  g <- attr(x, "edges")
+  g <- graph_from_edgelist(
     cbind(from = unlist(g[["from"]]), to = rep(g[["to"]], lengths(g[["from"]])))
   )
   igraph::distances(g, from, ...)[vec_data(x)]
@@ -47,8 +43,6 @@ node_distance <- function(x, from, ...){
 #' Identify disjoint graphs by node
 #'
 #' @param x A graph
-#'
-#' @export
 node_disjoint_id <- function(x, ...) {
   igraph::components(graphvec_to_igraph(x))$membership
 }
